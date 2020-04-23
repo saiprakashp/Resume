7cd7c66e436b827ce563cf07995f13e9ab4c0c57
saiprakashp <pulipakasaiprakash@gmail.com>
2020-04-23 20:38:55 +0530

    Resumne

---
+++ precache-manifest.eb7546748a52ed70fca613b5b19c6ef6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b90631e6553ea00b0e231faab0e95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b791f3367106eff3f4dd",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "d3f84fb1ec2d51e3c1ef",
+    "url": "/static/css/main.21dc34d1.chunk.css"
+  },
+  {
+    "revision": "b791f3367106eff3f4dd",
+    "url": "/static/js/2.31e13673.chunk.js"
+  },
+  {
+    "revision": "e0bf3cddf4d71102ce8ed1725ee95fc5",
+    "url": "/static/js/2.31e13673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f84fb1ec2d51e3c1ef",
+    "url": "/static/js/main.2d12c8fa.chunk.js"
+  },
+  {
+    "revision": "d94c63c5f6cc4de6b011",
+    "url": "/static/js/runtime-main.32a1fd03.js"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Black.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-BlackItalic.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Bold.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-BoldItalic.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Italic.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Light.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-LightItalic.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Medium.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-MediumItalic.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-Thin.d41d8cd9.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/static/media/Roboto-ThinItalic.d41d8cd9.ttf"
+  }
+]);
